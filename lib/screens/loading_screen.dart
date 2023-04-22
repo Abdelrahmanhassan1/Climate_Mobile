5b9d9bf24de70a1d2a90c4cc14c8c4c8aa957668
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2023-04-23 01:40:56 +0200

    adding API call and fetch Data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: library_private_types_in_public_api
 
+import 'package:climate_app/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:climate_app/services/location.dart';
 
@@ -11,11 +12,26 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude = 0.0;
+  double longitude = 0.0;
   @override
   void initState() {
     super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
     Location location = Location();
-    location.getCurrentLocation();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=09a61843625d8bf5ccc002273ad8a5b8");
+
+    var weatherData = await networkHelper.getData();
+
+    print(weatherData);
   }
 
   @override
